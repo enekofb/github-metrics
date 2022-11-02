963c91a19c86d81249a744685dfb70c0a6d38b02
Eneko Fernández <12957664+enekofb@users.noreply.github.com>
2022-11-02 18:53:20 +0000

    Merge pull request #2
    * added prometheus endpont
    
    * created metrics exporter

--- main.go
+++ main.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"github.com/enekofb/metrics/pkg/metrics"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+	"time"
+)
+
+var (
+	defectsLastMonth = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "defects_last_month",
+		Help: "defects last month",
+	})
+)
+
+func recordMetrics() {
+	go func() {
+		for {
+			numDefects, err := metrics.GetLastMonthDefectMetricsByTeam()
+			if err != nil {
+				panic(err)
+			}
+			defectsLastMonth.Set(float64(numDefects))
+			time.Sleep(2 * time.Second)
+		}
+	}()
+}
+
+func main() {
+	recordMetrics()
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":2112", nil)
+}
